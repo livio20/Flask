7107edc377a7c97898cd3b5dfd638c3472b3987b
livio20 <solotchi.livio@iisgalvanimi.edu.it>
2022-02-22 12:29:47 +0000

    primo commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/', methods=["GET"])
+def hello_world():
+  return render_template('index.html')
+
+
+@app.route('/it', methods=["GET"])
+def ciao_mondo():
+  return ('<h1>ciao, mondo!</h1>')
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
