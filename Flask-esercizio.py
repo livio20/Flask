28a46974dfbbca9d83974207878d4c11f6624ab6
livio20 <solotchi.livio@iisgalvanimi.edu.it>
2022-02-24 10:55:05 +0000

    quasi finito es 2

--- Flask-esercizio.py
+++ Flask-esercizio.py
@@ -1,20 +1,28 @@
+from datetime import datetime
 from flask import Flask, render_template
 app = Flask(__name__)
-
-from datetime import datetime
-
-
 
 
 @app.route('/')
 def hello_world():
-  return render_template('flask-esercizio-risposta.html')
+    return render_template('flask-esercizio-risposta.html')
+
 
 @app.route('/meteo')
-def stica():
-  return render_template('meteo.html')
+def meteo():
+    return render_template('meteo.html')
 
+
+@app.route('/frasicelebri')
+def libro():
+    return render_template('meteo.html')
+
+
+
+@app.route('/quantomanca')
+def calendario():
+    return render_template('meteo.html')
 
 
 if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=3245, debug=True)
+    app.run(host='0.0.0.0', port=3245, debug=True)
