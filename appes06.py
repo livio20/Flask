10b178b0be8fa5e798785f335587c7cf542b581d
livio20 <solotchi.livio@iisgalvanimi.edu.it>
2022-03-23 13:57:17 +0000

    es3

--- appes06.py
+++ appes06.py
@@ -3,23 +3,24 @@
 # cliccando su quaesto link si deve visuallizare la mappa dei quartieri di milano
 
 
-from flask import Flask, render_template, send_file, make_response, url_for, Response
+import matplotlib.pyplot as plt
+import matplotlib
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+import contextily
+import geopandas as gpd
+import io
+from flask import Flask, render_template, send_file, make_response, url_for, Response,request
 app = Flask(__name__)
 
-import io
-import geopandas as gpd
-import contextily
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from matplotlib.figure import Figure
-import matplotlib
 matplotlib.use('Agg')
-import matplotlib.pyplot as plt
 
 
 matplotlib.use('Agg')
 
 
-quartieri = gpd.read_file('/workspace/Flask/ds964_nil_wm (1)-20220322T111443Z-001.zip')
+quartieri = gpd.read_file(
+    '/workspace/Flask/ds964_nil_wm (1)-20220322T111443Z-001.zip')
 
 
 @app.route('/', methods=['GET'])
@@ -27,29 +28,53 @@ def home():
     return render_template('homepage.html')
 
 
-
-
-@app.route('/quartieri.png', methods=['GET'])
+@app.route('/visualizza.png', methods=['GET'])
 def quartieri1():
 
-    fig, ax = plt.subplots(figsize = (12,8))
+    fig, ax = plt.subplots(figsize=(12, 8))
 
-    quartieri.to_crs(epsg=3857).plot(ax=ax, alpha=0.5)
+    quartieri.to_crs(epsg=3857).plot(ax=ax, alpha=0.5, edgecolor='k')
     contextily.add_basemap(ax=ax)
     output = io.BytesIO()
     FigureCanvas(fig).print_png(output)
     return Response(output.getvalue(), mimetype='image/png')
 
 
-@app.route('/quartieri', methods=("POST", "GET"))
+@app.route('/visualizza', methods=("POST", "GET"))
 def mpl():
-    return render_template('quartieri2.html',
-                           PageTitle = "Matplotlib")
+    return render_template('visualizza.html',
+                           PageTitle="Matplotlib")
 
 
-@app.route('/cerca', methods=['GET'])
-def cerca1():
-    return render_template('cerca.html')
+
+
+@app.route('/form_ricerca', methods=['GET'])
+def ricerca_quartiere():
+    return render_template('form_ricerca.html') 
+
+
+
+@app.route('/ricerca.png', methods=['GET'])
+def ricerca_quart():
+    
+    fig, ax = plt.subplots(figsize=(12, 8))
+
+    quartiere.to_crs(epsg=3857).plot(ax=ax, alpha=0.5, edgecolor='k')
+    contextily.add_basemap(ax=ax)
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+
+@app.route('/ricerca', methods=("POST", "GET"))
+def cerca():
+    global quartiere
+    ricerca = request.args["Quartiere"]
+    quartiere = quartieri[quartieri["NIL"].str.contains(ricerca.upper())]
+
+    return render_template('plot2.html',
+                       PageTitle="Matplotlib")
+
 
 
 
