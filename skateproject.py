bbbafd196cfa180f6c6d38146dbebcfbee9c32e2
livio20 <solotchi.livio@iisgalvanimi.edu.it>
2022-04-07 09:43:45 +0000

    im dead

--- skateproject.py
+++ skateproject.py
@@ -2,50 +2,82 @@ from flask import Flask, render_template, request, Response
 app = Flask(__name__)
 
 
-
 @app.route("/", methods=["GET"])
 def scelta():
-    return render_template("scelta.html")
+    return render_template("home.html")
 
 
+@app.route("/home", methods=["GET"])
+def home():
+    return render_template("home.html")
 
 
+@app.route("/storia", methods=["GET"])
+def storia():
+    return render_template("storia.html")
 
 
+@app.route("/tavole", methods=["GET"])
+def tavole():
+    return render_template("tavole.html")
 
 
+@app.route("/truck", methods=["GET"])
+def truck():
+    return render_template("truck.html")
 
 
+@app.route("/ruote", methods=["GET"])
+def ruote():
+    return render_template("ruote.html")
 
 
+@app.route("/cuscinetti", methods=["GET"])
+def cuscinetti():
+    return render_template("cuscinetti.html")
 
 
+@app.route("/hardware", methods=["GET"])
+def hardware():
+    return render_template("hardware.html")
 
 
+@app.route("/grip_tape", methods=["GET"])
+def grip():
+    return render_template("grip_tape.html")
 
 
+@app.route("/tool", methods=["GET"])
+def tool():
+    return render_template("tool.html")
 
 
+@app.route("/wax", methods=["GET"])
+def wax():
+    return render_template("wax.html")
 
 
+@app.route("/guida_tavole", methods=["GET"])
+def guida_tavole():
+    return render_template("guida_tavole.html")
 
 
+@app.route("/guida_truck", methods=["GET"])
+def guida_truck():
+    return render_template("guida_truck.html")
 
 
+@app.route("/guida_ruote", methods=["GET"])
+def guida_ruote():
+    return render_template("guida_ruote.html")
 
 
-
-
-
-
-
-
-
-
-
+@app.route("/contatti", methods=["GET"])
+def contatti():
+    return render_template("contatti.html")
 
 
 
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=3245, debug=True)
+    app.run(host='0.0.0.0', port=3245, debug=True)
